45fbca7e448e150d1934af52ed6392768df6237a
Michael Wender <michael@michaelwender.com>
2020-11-04 13:22:42 -0500

    First commit

---
+++ chalkagency-extras.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Chalk Agency Extras
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Additions and mods to the Chalk Agency website.
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     chalkagency-extras
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Chalkagency_Extras
+ */
+
+// Your code starts here.
